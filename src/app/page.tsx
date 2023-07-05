6bebbfb62f1e9ff77be9f8e18de3e4bd0d6bb2b6
Jeison Tomio <medriase@gmail.com>
2023-07-04 22:24:19 -0300

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 export default function Home() {
+  const { data } = useSession()
+
   return (
-    <h1 className="flex justify-center items-center h-screen">Home page</h1>
+    <div>
+      <div className="flex flex-col">
+        <button onClick={() => signIn('google')}>Login com o Google</button>
+        <button onClick={() => signOut()}>Logout</button>
+      </div>
+      <h1>Olá, {data?.user?.name}</h1>
+      <img src={data?.user?.image ?? 'User'} alt="" />
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#590bd8',
+        secundary: '#312a4f',
+        primaryDarker: '#312a4f',
+      },
+      textColor: {
+        dark: '#717171',
+      },
     },
   },
   plugins: [],
