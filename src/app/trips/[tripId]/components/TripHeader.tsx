85a1a09f9671c7fc51c3df9f436b92bfdd2fac1a
Jeison Tomio <medriase@gmail.com>
2023-07-06 19:33:28 -0300

    feat: add initial trip details page and trip header

---
+++ src/app/trips/[tripId]/components/TripHeader.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactCountryFlag from 'react-country-flag'
+import Image from 'next/image'
+import { Trip } from '@prisma/client'
+
+interface TripHeaderProps {
+  trip: Trip
+}
+
+export default function TripHeader({ trip }: TripHeaderProps) {
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[280px] w-full">
+        <Image src={trip?.coverImage} fill objectFit="cover" alt={trip.name} />
+      </div>
+      {/* Title and Information */}
+      <div className="flex flex-col">
+        <h1 className="font-semibold text-xl text-primaryDarker">
+          {trip.name}
+        </h1>
+        <div className="flex items-center gap-1 my-1">
+          <ReactCountryFlag countryCode={trip.countryCode} svg />
+          <p className="text-xs text-grayPrimary underline">{trip.location}</p>
+        </div>
+        <p className="text-xs text-grayPrimary">
+          <span className="text-primary font-medium">
+            R${trip.pricePerDay.toString()}{' '}
+          </span>
+          pernoite
+        </p>
+      </div>
+    </div>
+  )
+}
